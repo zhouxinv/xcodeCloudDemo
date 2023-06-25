6e7297a1d34ef65381b61ea2faaf130dbd46628d
葛巍 <1246403921@qq.com>
2023-06-25 14:29:41 +0800

    * Upload files

---
+++ XcodeCloudTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeCloudTest
+//
+//  Created by 葛巍 on 2023/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
